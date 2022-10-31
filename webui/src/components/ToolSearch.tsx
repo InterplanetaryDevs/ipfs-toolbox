a92088f0b8c375501002e59d6857a08dacb32e6d
Undying Wraith <undyingwraith@outlook.com>
2022-10-31 15:46:55 +0100

    Improve tool search

--- webui/src/components/ToolSearch.tsx
+++ webui/src/components/ToolSearch.tsx
@@ -8,16 +8,22 @@ export function ToolSearch() {
 	const {setSearchOpen, isSearchOpen, setTool} = useToolBox();
 
 	const [value, setValue] = useState('');
+	const [selected, setSelected] = useState(0);
 
 	useEffect(() => {
 		if (isSearchOpen) {
-			setTimeout(() => document.getElementById('tool-search')?.focus(), 25);
+			setTimeout(() => {
+				const el = document.getElementById('tool-search') as HTMLInputElement;
+				el?.focus();
+				el?.setSelectionRange(0, value.length);
+			}, 25);
 		}
 	}, [isSearchOpen]);
 
 	const results = useMemo(() => {
 		const allItems = ([] as ITool[]).concat(...TOOLS.map(c => c.tools));
 
+		setSelected(0);
 		return allItems.filter(t => t.name.toLowerCase().includes(value.toLowerCase()));
 	}, [value]);
 
@@ -28,10 +34,31 @@ export function ToolSearch() {
 
 	return <Modal open={isSearchOpen}>
 		<Paper>
-			<TextField id={'tool-search'} value={value} onChange={e => setValue(e.target.value)} variant={'standard'}
-								 sx={{width: '100%'}}/>
+			<TextField
+				id={'tool-search'}
+				value={value}
+				onChange={e => setValue(e.target.value)}
+				variant={'standard'}
+				sx={{width: '100%'}}
+				onKeyDown={(ev) => {
+					if (ev.key === 'Enter' && results.length >= selected) {
+						ev.preventDefault();
+						openTool(results[selected]);
+					} else if (ev.key === 'ArrowDown') {
+						ev.preventDefault();
+						if (selected < results.length) {
+							setSelected(s => s + 1);
+						}
+					} else if (ev.key === 'ArrowUp') {
+						ev.preventDefault();
+						if (selected > 0) {
+							setSelected(s => s - 1);
+						}
+					}
+				}}
+			/>
 			<List>
-				{results.map((t, k) => (<ListItem>
+				{results.map((t, k) => (<ListItem key={t.name} selected={selected === k}>
 					<ListItemButton onClick={() => openTool(t)}>
 						<ListItemIcon>{t.icon ?? DefaultToolIcon}</ListItemIcon>
 						<ListItemText primary={t.name}/>

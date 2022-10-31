c96a95db3d14cded3ae5057b9fe70c6fb6adc2f2
Undying Wraith <undyingwraith@outlook.com>
2022-10-31 17:01:31 +0100

    Add basic files tool

--- webui/src/tools/TOOLS.tsx
+++ webui/src/tools/TOOLS.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import {EmbedTool} from './EmbedTool';
 import DashboardIcon from '@mui/icons-material/Dashboard';
@@ -10,20 +9,6 @@ import {
 	IpnsToolDefinition
 } from './definitions';
 import {FilesToolDefinition} from './definitions/FilesToolDefinition';
-=======
-import React, {lazy} from 'react';
-import {ITool} from '../App';
-import FolderIcon from '@mui/icons-material/Folder';
-import {EmbedTool} from './EmbedTool';
-import LanIcon from '@mui/icons-material/Lan';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-
-const IpnsTool = lazy(() => import('./ipns/IpnsTool'));
-export const IpnsToolDefinition: ITool = {
-	name: 'IPNS',
-	tool: <IpnsTool/>,
-};
->>>>>>> ad67671 (Add tool search)
 
 export const IpfsWebUIDefinition: ITool = {
 	name: 'IPFS WebUI',
@@ -43,11 +28,4 @@ export const TOOLS: IToolCategory[] = [
 	{name: 'Official', tools: [IpfsWebUIDefinition, IpfsCidToolDefinition]},
 	{name: 'Tools', tools: [FilesToolDefinition, IpnsToolDefinition, IpfsClusterToolDefinition]},
 ];
-<<<<<<< HEAD
 export const AdditionalTools = [DashboardDefinition, ConfigurationDefinition];
-=======
-
-export const DefaultToolIcon = <DashboardIcon/>;
-export const DefaultToolImage = '/assets/missing.jpg';
->>>>>>> ad67671 (Add tool search)
-

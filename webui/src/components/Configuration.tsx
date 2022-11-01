808c243fbaa57fbad72913b2baaa9ff8703c3075
Undying Wraith <undyingwraith@outlook.com>
2022-11-01 13:03:46 +0100

    Add configuration view

---
+++ webui/src/components/Configuration.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import {useIpfs} from '../context/IpfsContext';
+import {useEffectCancel} from '../hooks/UseEffectCancel';
+import {Container, Typography} from '@mui/material';
+
+export default function Configuration() {
+	const {ipfs} = useIpfs();
+	const [configuration, setConfiguration] = useState<any>();
+
+	useEffectCancel((signal) => {
+		ipfs.config.getAll({signal})
+			.then(setConfiguration)
+			.catch(console.error);
+	}, []);
+
+	return <Container>
+		<Typography variant={'h3'}></Typography>
+		<pre>{JSON.stringify(configuration, null, 2)}</pre>
+	</Container>;
+}

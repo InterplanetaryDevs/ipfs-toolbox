c23ce23539ae757738674c2df1db5ddb87d9d166
Undying Wraith <undyingwraith@outlook.com>
2023-11-08 11:44:45 +0100

    Update StatusTool.tsx

--- webui/src/tools/StatusTool.tsx
+++ webui/src/tools/StatusTool.tsx
@@ -1,26 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import {ConnectionChecker} from '../components/ConnectionChecker';
 import {useIpfs} from '../context/IpfsContext';
-import {Card, CardContent} from '@mui/material';
+import {Card, CardContent, CircularProgress} from '@mui/material';
 import {ToolContainer} from '../components/ToolContainer';
 import {useEffectCancel} from '../hooks/UseEffectCancel';
 
 export default function StatusTool() {
 	const [idInfo, setIdInfo] = useState<any>();
-	const ipfs = useIpfs()
+	const ipfs = useIpfs();
 
 	useEffectCancel((signal) => {
-		ipfs.node.id({signal}).then(setIdInfo)
-		//TODO: get id and such
+		ipfs.node.id({signal}).then(setIdInfo);
 	}, []);
 
 	return <ConnectionChecker context={ipfs}>
 		<ToolContainer>
 			<Card>
 				<CardContent>
-					{JSON.stringify(idInfo)}
+					{idInfo ? <>
+						<p>Id: {idInfo.id.toString()}</p>
+						<p>Version: {idInfo.agentVersion}</p>
+						<p>Addresses: <ul>{idInfo.addresses?.map((i: any) => <li>{i.toString()}</li>)}</ul></p>
+					</> : <CircularProgress/>}
 				</CardContent>
 			</Card>
 		</ToolContainer>
-	</ConnectionChecker>
+	</ConnectionChecker>;
 }

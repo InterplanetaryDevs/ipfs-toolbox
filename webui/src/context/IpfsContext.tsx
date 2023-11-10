9f95d48355abc25f96632f5a6d093e04cbb82009
Undying Wraith <undyingwraith@outlook.com>
2023-11-10 14:04:39 +0100

    Add In/Out Graph

--- webui/src/context/IpfsContext.tsx
+++ webui/src/context/IpfsContext.tsx
@@ -32,10 +32,14 @@ export function IpfsContextProvider(props: PropsWithChildren<{ create: (url: str
 			<Alert severity={'error'}>Could not connect to IPFS node!</Alert>
 			<Alert severity={'info'}>
 				<p>Try running the code below to update the IPFS API headers.</p>
-				<code>ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["https://ipfs-toolbox.on.fleek.co",
-					"http://ipfs-toolbox.on.fleek.co.localhost:8080", "http://localhost:3000", "http://127.0.0.1:5001",
-					"https://webui.ipfs.io"]'
-					ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'</code>
+				<code>
+					<p>
+						ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["https://ipfs-toolbox.on.fleek.co",
+						"http://ipfs-toolbox.on.fleek.co.localhost:8080", "http://localhost:3000", "http://127.0.0.1:5001",
+						"https://webui.ipfs.io"]'
+					</p>
+					<p>ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'</p>
+				</code>
 			</Alert>
 		</Stack>,
 	}}>
--- webui/src/tools/StatusTool.tsx
+++ webui/src/tools/StatusTool.tsx
@@ -1,9 +1,11 @@
-import {Card, CardContent, CircularProgress} from '@mui/material';
-import React, {useState} from 'react';
+import {Card, CardContent, CardHeader, CircularProgress, Stack} from '@mui/material';
+import Chart from 'chart.js/auto';
+import React, {useEffect, useRef, useState} from 'react';
 import {ConnectionChecker} from '../components/ConnectionChecker';
 import {ToolContainer} from '../components/ToolContainer';
 import {useIpfs} from '../context/IpfsContext';
 import {useEffectCancel} from '../hooks/UseEffectCancel';
+
 
 export default function StatusTool() {
 	const [idInfo, setIdInfo] = useState<any>();
@@ -15,15 +17,74 @@ export default function StatusTool() {
 
 	return <ConnectionChecker context={ipfs}>
 		<ToolContainer>
-			<Card>
-				<CardContent>
-					{idInfo ? <>
-						<p>Id: {idInfo.id.toString()}</p>
-						<p>Version: {idInfo.agentVersion}</p>
-						<p>Addresses: <ul>{idInfo.addresses?.map((i: any) => <li>{i.toString()}</li>)}</ul></p>
-					</> : <CircularProgress/>}
-				</CardContent>
-			</Card>
+			<Stack spacing={2}>
+				<Card>
+					<CardContent>
+						{idInfo ? <>
+							<p>Id: {idInfo.id.toString()}</p>
+							<p>Version: {idInfo.agentVersion}</p>
+							<p>Addresses: <ul>{idInfo.addresses?.map((i: any) => <li>{i.toString()}</li>)}</ul></p>
+						</> : <CircularProgress/>}
+					</CardContent>
+				</Card>
+				<Card>
+					<CardHeader title={'Network'}/>
+					<CardContent>
+						<ConnectionChart/>
+					</CardContent>
+				</Card>
+			</Stack>
 		</ToolContainer>
 	</ConnectionChecker>;
+}
+
+function ConnectionChart() {
+	const {node} = useIpfs();
+	const chartRef = useRef<HTMLCanvasElement>(null);
+	const [chart, setChart] = useState<Chart>();
+
+	// Load stats
+	useEffectCancel((signal) => {
+		if (chart) {
+			(async () => {
+				for await (const item of node.stats.bw({signal, interval: '1s', poll: true})) {
+					chart.data.labels.push(Date.now());
+					chart.data.datasets[0].data.push(item.rateIn);
+					chart.data.datasets[1].data.push(-item.rateOut);
+					chart.update();
+				}
+			})();
+		}
+	}, [chart]);
+
+	// Render graph
+	useEffect(() => {
+		let chart: Chart;
+		if (chartRef.current) {
+			chart = new Chart(chartRef.current, {
+				type: 'line',
+				data: {
+					labels: [],
+					datasets: [
+						{
+							label: 'Inbound',
+							data: [],
+						},
+						{
+							label: 'Outbound',
+							data: [],
+						},
+					],
+				},
+				options: {},
+			});
+			setChart(chart);
+		}
+
+		return () => {
+			chart?.destroy();
+		};
+	}, []);
+
+	return <canvas ref={chartRef}/>;
 }
